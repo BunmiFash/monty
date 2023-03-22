9d9be4566e13800e77ae20dcbcabd6371293c19b
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-03-22 22:34:51 +0000

    Fixed betty

--- files.c
+++ files.c
@@ -87,20 +87,22 @@ void find_func(char *opcode, char *value, int ln, int format)
 		{"push", add_begin},
 		{"pall", print}
 	};
-		/*{"pint", print_top_of_stack}
-		{"pop", pop_top},
-		{"nop", nop},
-		{"swap", swap_nodes},
-		{"add", add_nodes},
-		{"sub", sub_nodes},
-		{"div", div_nodes},
-		{"mul", mul_nodes},
-		{"mod", mod_nodes},
-		{"pchar", print_char},
-		{"pstr", print_str},
-		{"rotl", rotl},
-		{"rotr", rotr},
-		{NULL, NULL}*/
+	/*
+		*{"pint", print_top_of_stack}
+		*{"pop", pop_top},
+		*{"nop", nop},
+		*{"swap", swap_nodes},
+		*{"add", add_nodes},
+		*{"sub", sub_nodes},
+		*{"div", div_nodes},
+		*{"mul", mul_nodes},
+		*{"mod", mod_nodes},
+		*{"pchar", print_char},
+		*{"pstr", print_str},
+		*{"rotl", rotl},
+		*{"rotr", rotr},
+		*{NULL, NULL}
+		*/
 
 	if (opcode[0] == '#')
 		return;
